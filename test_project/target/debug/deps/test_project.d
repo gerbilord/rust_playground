76a5dbc9245dc61233b6ced02492091214ac4f70
Alessandro Cois <cois.alessandro@gmail.com>
2023-03-29 13:58:10 -0700

    First commit

---
+++ test_project/target/debug/deps/test_project.d
@@ -0,0 +1,5 @@
+C:\alt_root\Coding\rust\rust_playground\test_project\target\debug\deps\test_project.exe: src\main.rs
+
+C:\alt_root\Coding\rust\rust_playground\test_project\target\debug\deps\test_project.d: src\main.rs
+
+src\main.rs:
---
+++ test_project/target/debug/test_project.d
@@ -0,0 +1,1 @@
+C:\alt_root\Coding\rust\rust_playground\test_project\target\debug\test_project.exe: C:\alt_root\Coding\rust\rust_playground\test_project\src\main.rs
